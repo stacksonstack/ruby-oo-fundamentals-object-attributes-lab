09858b47a4569e6e3f6bb2815bc30d8b1b7f65a8
stacksonstack <staceycarrillo44@gmail.com>
2020-09-15 13:01:42 -0400

    finished

---
+++ lib/dog.rb
@@ -0,0 +1,22 @@
+class Dog 
+
+    def name=(name)
+        @name = name
+    end
+
+    def name
+        @name
+    end 
+    
+    def breed=(dog)
+        @breed = dog
+    end
+
+    def breed
+        @breed
+    end
+end
+
+
+
+    
